b2d4901264e8332c2f3fb1ef3680a919a2645bad
Michael Alt <mail@lean-stack.de>
2020-11-25 23:35:55 +0100

    Add missing framework prop.

--- src/app/models/user.interface.ts
+++ src/app/models/user.interface.ts
@@ -1,7 +1,10 @@
+import {Framework} from './framework.interface';
+
 export interface User {
   id?: number;
   account: string;
   email: string;
   password: string;
   zip?: string;
+  framework: Framework;
 }
